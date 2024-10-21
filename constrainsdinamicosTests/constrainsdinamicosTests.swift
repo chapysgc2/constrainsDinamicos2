0690e186c3a39db8436de5874120484e012b9649
hazelgc2 <hazelo235@gmail.com>
2024-10-20 20:16:25 -0600

    Initial Commit

---
+++ constrainsdinamicosTests/constrainsdinamicosTests.swift
@@ -0,0 +1,17 @@
+//
+//  constrainsdinamicosTests.swift
+//  constrainsdinamicosTests
+//
+//  Created by Hazel Alain on 20/10/24.
+//
+
+import Testing
+@testable import constrainsdinamicos
+
+struct constrainsdinamicosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
